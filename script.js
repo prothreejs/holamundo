5b161b767ad8e462835e6e235981161250c90561
blackopsdev0 <blackopsdevs@gmail.com>
2024-05-05 00:54:47 -0600

    Update
    Update

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 camera.position.z = 5;
+
 function animate() { 
 requestAnimationFrame( animate );
 cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 renderer.render( scene, camera );
 }
+
 animate();
